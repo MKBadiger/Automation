2f028ea4664c74ba4e1d5a808c01022e10f113b9
mahesh005 <maheshbadiger86@gmail.com>
2017-09-09 11:47:50 +0530

    Update test.java

--- testNG_Flipkart/src/test/java/com/Gmail/test.java
+++ testNG_Flipkart/src/test/java/com/Gmail/test.java
@@ -11,11 +11,11 @@ public class test {
 		WebDriver driver=new FirefoxDriver();
 		driver.get("http://google.com");
 		driver.findElement(By.xpath("//a[@class='gb_P']")).click();
-		driver.findElement(By.id("Email")).sendKeys("maheshbadiger86@gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("m@gmail.com");
 		Thread.sleep(2000);
 		driver.findElement(By.id("next")).click();
 		Thread.sleep(2000);
-		driver.findElement(By.id("Passwd")).sendKeys("m<23091990");
+		driver.findElement(By.id("Passwd")).sendKeys("*********");
 		Thread.sleep(2000);
 		driver.findElement(By.id("signIn")).click();
 		Thread.sleep(5000);
